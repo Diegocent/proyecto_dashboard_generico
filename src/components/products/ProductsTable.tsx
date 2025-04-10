1af989f4a7139d1c44541213949f08794a54d23d
dvillalba1 <dvillalba@excelsis.com.py>
2025-04-09 21:10:55 -0300

    se agrego la  tabla

--- src/components/products/ProductsTable.tsx
+++ src/components/products/ProductsTable.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Edit, Search, Trash2 } from "lucide-react";
+import { filter } from "framer-motion/client";
 
 const PRODUCT_DATA = [
   {
@@ -46,6 +47,18 @@ const PRODUCT_DATA = [
 ];
 
 export const ProductsTable = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterProducts, setFilterProducts] = useState(PRODUCT_DATA);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = PRODUCT_DATA.filter(
+      (product) =>
+        product.name.toLowerCase().includes(term) ||
+        product.category.toLowerCase().includes(term)
+    );
+    setFilterProducts(filtered);
+  };
   return (
     <motion.div
       className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl p-6 border-gray-700 mb-8"
@@ -61,9 +74,73 @@ export const ProductsTable = () => {
             type="text"
             placeholder="Search products..."
             className="bg-gray-700 text-white placeholder-gray-400 rounded-lg pl-10 pr-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={searchTerm}
+            onChange={handleSearch}
           />
           <Search className="absolute top-3 left-3 text-gray-400" size={18} />
         </div>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-700">
+          <thead>
+            <tr>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Category
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Price
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Stock
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Sales
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
+                Actions
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-700">
+            {filterProducts.map((product) => (
+              <motion.tr
+                key={product.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.1 }}
+                className="hover:bg-gray-700 cursor-pointer"
+              >
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-100 font-medium flex gap-2 items-center">
+                  <img
+                    src="https://plus.unsplash.com/premium_photo-1731948133366-0d1dbb7db851?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
+                    alt="Product img"
+                    className="size-10 rounded-full"
+                  />
+                  {product.name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-300">
+                  {product.category}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-100">
+                  ${product.price.toFixed(2)}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-100">
+                  {product.stock}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-100">
+                  {product.sales}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-100">
+                  <Edit className="text-blue-500 cursor-pointer" size={18} />
+                  <Trash2 className="text-red-500 cursor-pointer" size={18} />
+                </td>
+              </motion.tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </motion.div>
   );
